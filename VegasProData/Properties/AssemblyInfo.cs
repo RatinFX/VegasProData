c4158641f0adb1bfbad890c1a884f1616bb5fafb
ratina0 <ratina0@gmail.com>
2023-03-12 16:50:43 +0100

    rebranding, version, CustomColors

--- VegasProData/Properties/AssemblyInfo.cs
+++ VegasProData/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("VegasProData")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("RatinA0")]
+[assembly: AssemblyCompany("RatinFX")]
 [assembly: AssemblyProduct("VegasProData")]
-[assembly: AssemblyCopyright("Copyright © RatinA0 2022-2023")]
+[assembly: AssemblyCopyright("Copyright © RatinFX 2022-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
---
+++ VegasProData/Theme/CustomColors.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace VegasProData.Theme
+{
+    /// <summary>
+    /// Used to overwrite `ThemeController` colors
+    /// </summary>
+    public static class CustomColors
+    {
+        /// <summary>
+        /// RGB 17, 93, 137
+        /// </summary>
+        public static readonly Color ApplyBlue = Color.FromArgb(16, 93, 137);
+
+        /// <summary>
+        /// RGB 70, 70, 70
+        /// </summary>
+        public static readonly Color VegasGray = Color.FromArgb(70, 70, 70);
+
+        /// <summary>
+        /// RGB 45, 45, 45
+        /// </summary>
+        public static readonly Color VegasDarkGray = Color.FromArgb(45, 45, 45);
+    }
+}
