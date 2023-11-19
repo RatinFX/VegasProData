ab3aa29bc29e87381a7d1a0f78cf62623d19c43f
ratina0 <ratina0@gmail.com>
2023-11-19 01:02:24 +0100

    Breaking - Moved some General methods to RatinFX.VP

--- VegasProData/General/Localization.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Globalization;
-
-namespace VegasProData.General
-{
-    public static class Localization
-    {
-        public static CultureInfo Current = CultureInfo.GetCultureInfo("en-US");
-    }
-}
